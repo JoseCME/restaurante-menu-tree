25613d149522ecf9dfdff258542d920a33472409
JoseCME <lollolllonuevocanal@gmail.com>
2026-05-25 21:58:58 -0600

    build(app): agregar plugin OpenAPI Generator, dependencia jackson-databind-nullable y NodeMapper

--- restaurant-menu-tree/app/src/main/java/gt/edu/miumg/app/repository/MongoNodeRepository.java
+++ restaurant-menu-tree/app/src/main/java/gt/edu/miumg/app/repository/MongoNodeRepository.java
@@ -1,12 +1,12 @@
 package gt.edu.miumg.app.repository;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.List;
 import java.util.Optional;
 
+@ConditionalOnProperty(name = "app.storage", havingValue = "mongo")
 public interface MongoNodeRepository extends MongoRepository<NodeDocument, String> {
-
     Optional<NodeDocument> findByParentIdIsNull();
-
     List<NodeDocument> findByParentId(String parentId);
 }
